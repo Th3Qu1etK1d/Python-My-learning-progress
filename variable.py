47163eda6098b2c19b4f32d8a0d60867f822380f
Hugo <65466494+Th3Qu1etK1d@users.noreply.github.com>
2023-07-23 15:48:09 -0400

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,71 @@
+# Code from Youtube channel - Bro code - Python full Course for free
+# I'm a new person in the world of programming. Note that all code is not creat by me
+# my goal is to train myself and letting a trace of what i'm doing.
+# This is not copy and paste (Ctrl c + ctrl v) 
+
+# variable = a container for value. Behave as the value that it contains  
+
+
+#first_name = "Hugo"
+#last_name = "hi"
+#full_name = first_name +" "+ last_name
+#print("hugo "+ last_name)
+#print(type(first_name))
+
+#age = 65
+#age *= 3
+#print("Your age is: "+str(age) + " "+ "Years old")
+#print(type(age))
+
+#time = 15
+#print("In : "+ str(time) + " "+ "Minute" )
+#print(type(time))
+
+#forest = True
+#print("Is it a forest? : "+str(forest))
+#print(type(forest))
+
+#____________________________________________________________
+
+#multiple assigment = allows us to assign mltiple variables at the same time in one line of code
+
+#name = "Frog"
+#age = 2
+#attractive = False
+
+#name, age, attractive = "Frog", 2, False
+
+#print(name)
+#print(age)
+#print(attractive)
+
+#Nancy = 18
+#Bob = 18
+#Kory = 18
+
+#Nancy = Bob = Kory = 18
+
+#print(Nancy)
+#print(Bob)
+#print(Kory)
+
+#___________________________________________________
+
+# String methode
+
+#name = "Rainbow Zoo"
+
+#print(len(name))
+#print(name.find("z"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("n"))
+#print(name.replace("i" , "e")
+#print(name*99999)
+
+
+
+
