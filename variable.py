a371ed33a27fee4f2a5cefeafc0d9291970af72c
Hugo <65466494+Th3Qu1etK1d@users.noreply.github.com>
2023-07-23 15:48:31 -0400

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,5 +1,5 @@
 # Code from Youtube channel - Bro code - Python full Course for free
-# I'm a new person in the world of programming. Note that all code is not creat by me
+# I'm a new person in the world of programming. Note that all code is not create by me
 # my goal is to train myself and letting a trace of what i'm doing.
 # This is not copy and paste (Ctrl c + ctrl v) 
 
